e2e5422c4c0f4f49abd6e3a3a869c892a7f64c2e
hicinformatic <hicinformatic@gmail.com>
2019-12-15 12:24:24 +0000

    urls auth

--- apps/authenticate/urls.py
+++ apps/authenticate/urls.py
@@ -2,16 +2,23 @@
 from django.urls import path, include
 from mighty.apps.authenticate.views import EmailViewSet, SmsViewSet
 from mighty.apps.authenticate.views.authenticate import Logout, Login, LoginBasic, LoginSms, LoginEmail
+from mighty.apps.authenticate.apps import AuthenticateConfig
 
+
+
+
+loginviews = []
+if AuthenticateConfig.method.basic:
+    loginviews.append(path("basic/<uid>", LoginBasic.as_view(), name="login-basic"))
+if AuthenticateConfig.method.email:
+    loginviews.append(path("email/<uid>", LoginEmail.as_view(), name="login-email"))
+if AuthenticateConfig.method.sms:    
+    loginviews.append(path("sms/<uid>", LoginSms.as_view(), name="login-sms"))
+loginviews.append(path("", Login.as_view(), name="login-auth"))
 
 urlpatterns = [
     path('email/', include(EmailViewSet().urls)),
     path('sms/', include(SmsViewSet().urls)),
     path("logout/", Logout.as_view(), name="logout"),
-    path('login/', include([
-        path("basic/<uid>", LoginBasic.as_view(), name="login-basic"),
-        path("email/<uid>", LoginEmail.as_view(), name="login-email"),
-        path("sms/<uid>", LoginSms.as_view(), name="login-sms"),
-        path("", Login.as_view(), name="login-auth"),
-    ])),
+    path('login/', include(loginviews)),
 ]
